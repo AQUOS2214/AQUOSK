6044da29e736cde6742fb29d73d47c9eeff5798f
AQUOS <90078074+AQUOS2214@users.noreply.github.com>
2021-10-08 17:12:10 +0900

    Add files via upload

---
+++ chat.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>チャット</title>
+</head>
+<body>
+    <h1>
+        メイン
+    </h1>
+<p>投稿</p>
+<form>
+<input type="text" name="a"><input type="submit" value="送信">
+</form>
+
+<?php
+if(isset($_GET["a"])){
+ $f=fopen("chat.txt","at");
+ $mozi=htmlspecialchars($_GET["a"],ENT_QUOTES);
+ fwrite($f,$mozi."\n");
+ fclose($f);
+}
+
+print "<pre>";
+readfile("chat.txt");
+print "</pre>";
+?>
+
+</body>
+</html>
